ebf55e5648bef5ee80170b347d6e3abb2d2629f4
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-20 22:38:13 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -12,8 +12,9 @@ class CouponsController < ApplicationController
   end
 
   def create
-    coupon = Coupon.new(:coupon_code => params[:coupon][:coupon_code], :store => params[:coupon][:store])
-    coupon.save
+#    coupon = Coupon.new(:coupon_code => params[:coupon][:coupon_code], :store => params[:coupon][:store])
+    coupon = Coupon.create(params[:coupon])
+#    coupon.save
     redirect_to coupon_path(coupon)
   #  redirect_to coupons_path
   end
